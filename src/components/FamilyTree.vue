3b49649e862c77576272d5b79bc4f134cf4a0574
Vnext-NhanVu2 <nhanvu2@vnext.com.vn>
2020-04-27 11:59:54 +0700

    Add canvas

--- src/components/FamilyTree.vue
+++ src/components/FamilyTree.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     Family tree
+    <canvas id="c"></canvas>
   </div>
 </template>
 
@@ -8,20 +9,49 @@
   export default {
     name: "FamilyTree",
     props: ["value"],
+    data () {
+      return {
+        rectWidth: 30,
+      }
+    },
     watch: {
       // eslint-disable-next-line no-unused-vars
       'value.relations': function (newValue, oldValue) {
+        var self = this;
         // eslint-disable-next-line no-debugger
         Object.keys(newValue).forEach(function (root) {
           let tree = newValue[root];
-          this.buildTree(root, tree);
+          self.buildTree(root, tree);
         });
       }
+    },
+    mounted() {
+      var c = document.getElementById("c");
+      var ctx = c.getContext("2d");
+      this.vueCanvas = ctx;
     },
     methods: {
       // eslint-disable-next-line no-unused-vars
       buildTree(root, tree) {
+        this.drawRect();
+      },
+      drawRect() {
+        // eslint-disable-next-line no-debugger
+        // clear canvas
+        this.vueCanvas.clearRect(0, 0, 400, 200);
 
+        // draw rect
+        this.vueCanvas.beginPath();
+        this.vueCanvas.rect(20, 20, this.rectWidth, 100);
+        this.vueCanvas.stroke();
+      },
+      addWidth() {
+        this.rectWidth += 10;
+        this.drawRect()
+      },
+      subWidth() {
+        this.rectWidth -= 10;
+        this.drawRect()
       }
     }
   }
